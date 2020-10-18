525c1e7a295e26d02da58a6507d4008387cc834a
Sarielcrys <57373584+Sarielcrys@users.noreply.github.com>
2020-10-18 16:47:33 +0800

    Update PT.m

--- PT.m
+++ PT.m
@@ -1,21 +1,21 @@
 % PT-symmetric of double rings/one bus waveguide system
 % Source code by CHEN Nuo
-% 2020/10/13
+% 2020/10/15
 % -------------------------------------------------------------------------------------------------------------------------------------------
 %%
 clear all
 clc
 close all
 
-phase_detuning = linspace(-pi/12,pi/12,1001);                                 % scanning round-trip phase detuning
+phase_detuning = linspace(-pi/12,pi/12,1001);                               % scanning round-trip phase detuning
 
 % basic parameters
-radius = 80e-6;
+radius = 10e-6;
 c = 3e8;
 lambda0 = 1550e-9;
 
 n = lambda0*680/2/pi/radius;                                                % m = 680, n = 2.0969
-omega0 = 2*pi*(c/n)/lambda0;
+omega0 = 2*pi*c/lambda0;
 L = 2*pi*radius;                                                            % assuming 2 rings have equal radius
 T = L/(c/n);
 
@@ -25,17 +25,17 @@ r1 = sqrt(1-k1^2);
 
 % ring 1
 a1 = 0.98;                                                                  % assuming ring 1 is negtive
-alpha1 = -2*log(a1)/(pi*radius);
+alpha1 = -2*log(a1)/L;
 
 % position 2/ring 2 (coulping between 2 rings)
 k2 = linspace(0.005,0.2,1001);
 r2 = sqrt(1-k2.^2);
-kfixed = k2(500);
-r2fixed = sqrt(1-kfixed^2);
+k2fixed = k2(500);
+r2fixed = sqrt(1-k2fixed^2);
 a2 = linspace(0.96,1.2,1001);
-%za2fixed = a2(339);
-a2fixed = a2(600);
-alpha2 = -2*log(a2)/(pi*radius);
+%a2fixed = a2(339);                                                         % condition of PT-symmetic
+a2fixed = a2(280);
+alpha2 = -2*log(a2)/L;
 
 %%
 trans1 = zeros(length(phase_detuning),length(a2));
@@ -49,22 +49,22 @@ for ii = 1:length(phase_detuning)                                           % ka
 end
 
 %%
-% for ii = 1:length(phase_detuning)                                         % adjust resolution
-% 
-%     for jj = 1:length(a2)
-%         if trans1(jj,ii) >= 100
-%             trans1(jj,ii) = 100;
-%         end
-%         if trans1(jj,ii) <= 1e-2
-%             trans1(jj,ii) = 1e-2;
-%         end
-%     end
-% end
+for ii = 1:length(phase_detuning)                                           % adjust resolution
+
+    for jj = 1:length(a2)
+        if trans1(jj,ii) >= 100
+            trans1(jj,ii) = 100;
+        end
+        if trans1(jj,ii) <= 1e-2
+            trans1(jj,ii) = 1e-2;
+        end
+    end
+end
 
 %%
 figure(1)
 mesh(phase_detuning,a2,10*log10(trans1))
-title('Transmission spectrum as the function of gain (k2 = 1.032)')
+title('Transmission spectrum as the function of gain(k2 = 1.032)')
 colormap jet
 colorbar
 set(gca,'Xtick',(-pi/12:pi/24:pi/12))
@@ -86,21 +86,23 @@ for ii = 1:length(phase_detuning)
 end
 
 %%
-% for ii = 1:length(phase_detuning)
-%     for jj = 1:length(r2)
-%         if trans2(jj,ii) >= 10
-%             trans2(jj,ii) = 10;
-%         end
-%         if trans2(jj,ii) <= 1e-1
-%             trans2(jj,ii) = 1e-1;
-%         end
-%     end
-% end
+for ii = 1:length(phase_detuning)
+    for jj = 1:length(r2)
+        if trans2(jj,ii) >= 10
+            trans2(jj,ii) = 10;
+        end
+        if trans2(jj,ii) <= 1e-1
+            trans2(jj,ii) = 1e-1;
+        end
+    end
+end
 
 %%
 figure(2)
 mesh(phase_detuning,k2,10*log10(trans2))
-title('Transmission spectrum as the function of κ (a2 = 1.041,loss basicly equals gain)')
+title('Transmission spectrum as the function of κ(a2 = 1.041,loss basicly equals gain)')
+title('Transmission spectrum as the function of κ(a2 = 1.079)')
+title('Transmission spectrum as the function of κ(a2 = 1.027)')
 colormap jet
 colorbar
 set(gca,'Xtick',(-pi/12:pi/24:pi/12))
@@ -110,3 +112,34 @@ set(gca,'Ytick',(0:0.05:0.2))
 set(gca,'YtickLabel',{'0','0.05','0.10','0.15','0.20'})
 ylabel('κ')
 zlabel('Transmission (dB)')
+
+%% energy coupling formalisn
+
+% ring 1
+y1 = alpha1*c/n;
+kappa1 = k1/T;
+gamma1 = -2*log(r1)/T;
+
+% ring 2
+y2 = alpha2*c/n;
+kappa2 = k2fixed/T;
+gamma2 = -2*log(r2fixed)/T;
+
+omega1 = omega0-1i*(y1+gamma1+y2)/4+sqrt(16*kappa2.^2-(y1+gamma1-y2).^2)/4;
+omega2 = omega0-1i*(y1+gamma1+y2)/4-sqrt(16*kappa2.^2-(y1+gamma1-y2).^2)/4;
+
+figure(3)
+plot(a2,(real(omega1)-omega0)/1e9,'b.')
+hold on
+plot(a2,(real(omega2)-omega0)/1e9,'r.')
+title('\omega real')
+xlabel('gain (a2)')
+ylabel('\omega - \omega0 (GHz)')
+
+figure(4)
+plot(a2,imag(omega1)/1e9,'g.')
+hold on
+plot(a2,imag(omega2)/1e9,'y.')
+title('\omega image')
+xlabel('gain (a2)')
+ylabel('(GHz)')
